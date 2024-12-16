03633280fd6ef30b00e3d0891ec7ff64dda2bce0
Nimira43 <n.rabone72@aol.com>
2024-12-16 14:42:38 +0000

    Sending a JSON response

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 import express from 'express'
 
 const app = express()
-app.listen(5001, console.log(`Sever running on Port 5001`))
+
+app.get('/', (req, res) => {
+  res.json('Apothematos Management System, by NimiraTech')
+})
+
+console.log('Apothematos Management System, by NimiraTech')
+
+app.listen(5001, console.log(`Server running on Port 5001`))
