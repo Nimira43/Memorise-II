8de8b6b252f4d05a257cd8bb86f4eeed89f83612
Nimira43 <n.rabone72@aol.com>
2024-12-16 14:09:12 +0000

    Tailwind brought in and configured

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <h1 className='logo'>Apothématos</h1>
-      <h4 className='heading'>Heading</h4>
+      <h1 className='heading'>Heading</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, exercitationem?</p>
     </div>
   )
