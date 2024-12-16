55a0acb75bbe34408c3e090d5d07a411cca9d16b
Nimira43 <n.rabone72@aol.com>
2024-12-16 15:03:04 +0000

    Nodemon running - sending HTML content

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,36 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json('Apothematos Management System, by NimiraTech')
+  res.send(`
+    <style>
+      body {
+        background-color: black;
+      }
+      h1 {
+        font-family: 'Verdana';
+        font-weight: 400; 
+        background-color: black;
+        text-align: center;
+        text-transform: uppercase;
+        padding: 20px 10px;
+        border: 2px solid orangered;
+        border-radius: 10px;
+        color: gold;
+      }
+      p {
+        color: orangered;
+        font-family: 'Verdana';
+        font-weight: 400; 
+        text-align: center;
+      }
+    </style>
+
+    <div>
+      <h1>Apothematos Management System</h1>
+      <hr />
+      <p>by NimiraTech</p>
+    </div> 
+  `)
 })
 
 console.log('Apothematos Management System, by NimiraTech')
