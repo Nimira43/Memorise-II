c6b10486e1e2a40a6e4d87dcb2001f1b35683b21
Nimira43 <n.rabone72@aol.com>
2024-12-26 23:59:10 +0000

    Set up basic loadCards async method

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let cards = []
 
+loadCards()
 
+async function loadCards() {
+  try {
+    let response = await fetch('./data/card-info.json')
+    let cardsArray = await response.json()
+    console.log(cardsArray)
+  } catch (error) {
+    console.log(error)
+  }
+}
