a1036f073517cb5b190fe96079078f1193cd9a9d
Nimira43 <n.rabone72@aol.com>
2024-12-26 23:38:24 +0000

    Created data/card-info.json with the images for six cards

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let cards = []
 
-Reducing the number of images for the moment. Keeping   back remaining images as they'll be needed once the base project is complete. Then they'll be brought back in to expand the Game
+
