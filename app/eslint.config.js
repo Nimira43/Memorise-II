5893c1f8db4451b5c68a5375a45ca0ef409f77cf
Nimira43 <n.rabone72@aol.com>
2024-12-26 18:58:23 +0000

    Wrote eslint.config.js and created .prettierrc

---
+++ app/eslint.config.js
@@ -0,0 +1,12 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node }
+  },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+]
