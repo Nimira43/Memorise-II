91c21b374438478392dc86b4bc08f133e1fbf8c5
Nimira43 <n.rabone72@aol.com>
2024-12-27 00:28:20 +0000

    Using the fetch API with promises to handle retrieval

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let cards = []
 
-loadCards()
+fetch('./data/card-info.json')
+  .then(response => response.json())
+  .then((data) => {
 
-async function loadCards() {
-  try {
-    let response = await fetch('./data/card-info.json')
-    let cardsArray = await response.json()
-    console.log(cardsArray)
-  } catch (error) {
-    console.log(error)
-  }
-}
+    console.log(data)
+    
+  })
+  .catch((error) => {
+    console.log('Error fetching card data: ', error)
+  })
