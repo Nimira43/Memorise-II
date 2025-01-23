5ccd7b44776f87b936f67154de972e952091bc86
Nimira43 <n.rabone72@aol.com>
2025-01-23 07:05:28 +0000

    Added click event on card

--- script.js
+++ script.js
@@ -11,27 +11,37 @@ fetch('./data/card-info.json')
     console.log('Error fetching card data: ', error)
   })
 
-  function dealCards(cards) { 
-    let fragment = document.createDocumentFragment()
-    for (const card of cards) {
-      let cardElement = document.createElement('div')
-      cardElement.classList.add('card')
-      cardElement.setAttribute('data-name', card.name)
+function dealCards(cards) { 
+  let fragment = document.createDocumentFragment()
+  for (const card of cards) {
+    let cardElement = document.createElement('div')
+    cardElement.classList.add('card')
+    cardElement.setAttribute('data-name', card.name)
 
-      let frontCardDiv = document.createElement('div')
-      frontCardDiv.classList.add('front')
+    let frontCardDiv = document.createElement('div')
+    frontCardDiv.classList.add('front')
 
-      let backCardDiv = document.createElement('div')
-      backCardDiv.classList.add('back')
-      let img = document.createElement('img')
-      img.classList.add('back-image')
-      img.src = `${card.image}.png`
-      backCardDiv.appendChild(img)
+    let backCardDiv = document.createElement('div')
+    backCardDiv.classList.add('back')
+    let img = document.createElement('img')
+    img.classList.add('back-image')
+    img.src = `${card.image}.png`
+    backCardDiv.appendChild(img)
 
-      cardElement.append(backCardDiv, frontCardDiv)
-      fragment.appendChild(cardElement)
-    }
-    cardTable.appendChild(fragment)
+    cardElement.append(backCardDiv, frontCardDiv)
+    fragment.appendChild(cardElement)
   }
+  cardTable.appendChild(fragment)
+
+  let dealtCards = document.querySelectorAll('.card')
+  dealtCards.forEach(card => {
+    card.addEventListener('click', flipCard)
+  })
+}
+
+function flipCard() {
+  console.log('text')
+}
+
 
   
